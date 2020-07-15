1667ce1210c429feb25777bfd4b1528ee45107d4
ajh3000 <ajh3000@github.com>
2020-07-15 13:34:25 +0000

    added support for write, improved pretty printing

--- pretty.c
+++ pretty.c
@@ -29,12 +29,15 @@ pretty_prog(FILE *fp, Program prog) {
 
 void
 print_procs(FILE *fp, int indent, Procs procs) {
+
     fprintf(fp, "%*s ", indent, "proc"); 
     print_header(fp, indent, procs->p_first->p_header);
+
     if(procs->p_first->p_decls != NULL)
         print_decls(fp, procs->p_first->p_decls);
+    
     print_statements(fp, indent, procs->p_first->p_body);
-    fprintf(fp, "%*s", indent, "end\n\n"); 
+    fprintf(fp, "%*s", indent, "\nend\n\n"); 
     
     if(procs->p_rest != NULL)
         print_procs(fp, indent, procs->p_rest);
@@ -113,11 +116,12 @@ print_statements(FILE *fp, int indent, Stmts stmts) {
 
     if (stmts->s_first != NULL) {
         print_statement(fp, stmts->s_first); 
-        fprintf(fp, "\n");
     }
 
-    if (stmts->s_rest != NULL) 
+    if (stmts->s_rest != NULL) {
+        fprintf(fp, ";\n");
         print_statements(fp, indent, stmts->s_rest); 
+    }
 }
 
 void
@@ -133,7 +137,7 @@ print_statement(FILE *fp, Stmt stmt) {
         case STMT_BLOCK:
             fprintf(fp, "{\n");
             print_statements(fp, 2, stmt->s_info.s_block);
-            fprintf(fp, "}");
+            fprintf(fp, "\n}");
             break;
         case STMT_COND:
             fprintf(fp, "%s ", "if");
@@ -145,9 +149,10 @@ print_statement(FILE *fp, Stmt stmt) {
             break;
         case STMT_READ:
             fprintf(fp, "%s ", "read");
+            fprintf(fp, "%s", stmt->s_info.s_read);
             break;
         case STMT_SKIP:
-            fprintf(fp, "%s ", "skip");
+            fprintf(fp, "%s", "skip");
             break;
         case STMT_WHILE:
             fprintf(fp, "%s ", "while");
@@ -157,9 +162,15 @@ print_statement(FILE *fp, Stmt stmt) {
             break;
         case STMT_WRITE:
             fprintf(fp, "%s ", "write");
+            print_expression(fp, stmt->s_info.s_write);
             break;
         case STMT_FOR:
             fprintf(fp, "%s ", "for");
+            fprintf(fp, "%s ", stmt->s_info.s_for.for_id);
+            print_expression(fp, stmt->s_info.s_for.for_from_expr);
+            fprintf(fp, " %s ", "do");
+            print_expression(fp, stmt->s_info.s_for.for_to_expr);
+            print_statement(fp, stmt->s_info.s_for.for_body);
             break;
         case STMT_CALL:
             fprintf(fp, "%s(", stmt->s_info.s_call.call_id);
